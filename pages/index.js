3dfa0c667694156af9a1b29dd5e5fa1ca813e58f
Azura <azstima@gmail.com>
2021-10-10 15:02:23 +0700

    add google meta tag

--- pages/index.js
+++ pages/index.js
@@ -51,6 +51,7 @@ export default function Index({ projects }) {
           {/* eslint-disable-next-line react/no-unescaped-entities */}
           @import url('https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
         </style>
+        <meta name="google-site-verification" content="AVb08B566O3Wuy0Zbw5fbqUsZ_mLipveVsN13N-02dc" />
       </Head>
       <Container>
         <Stack
